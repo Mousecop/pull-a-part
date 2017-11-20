6aa2ea7553e8d452cc8ec891709276af2ae7c2e2
Mousecop <joaskew13@gmail.com>
2017-11-20 16:40:21 -0500

    heroku test

--- server.js
+++ server.js
@@ -2,9 +2,84 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
-
+const request = require('request');
 const app = express();
 const server = http.createServer(app);
+
+
+const DataOneURL = 'https://api.dataonesoftware.com/webservices/vindecoder/decode';
+const clientId = '3578';
+const authToken = '8c35d0f3436d10b99b54de831db5885f0bca1a5e';
+const dataOneQuery = JSON.stringify({
+  "decoder_settings" : {
+    "display" : "full",
+    "version" : "7.0.1",
+    "styles" : "off",
+    "common_data" : "on",
+    "common_data_packs" : {
+     "basic_data" : "on",
+     "pricing" : "off",
+     "engines" : "off",
+     "transmissions" : "off",
+     "specifications" : "off",
+     "standard_specifications" : "off",
+     "optional_specifications" : "off",
+     "standard_ancillary_specifications" : "off",
+     "optional_ancillary_specifications" : "off",
+     "installed_equipment" : "off",
+     "optional_equipment" : "off",
+     "colors" : "off",
+     "safety_equipment" : "off",
+     "warranties" : "off",
+     "fuel_efficiency" : "off",
+     "green_scores" : "off",
+     "crash_test" : "off",
+     "awards" : "off"
+     }
+ },
+ "query_requests" : {
+    "Request-Sample" : {
+     "vin" : "1FADP3E22DL309391",
+     "year" : "",
+     "make" : "",
+     "model" : "",
+     "trim" : "",
+     "model_number" : "",
+     "package_code" : "",
+     "drive_type" : "",
+     "vehicle_type" : "",
+     "body_type" : "",
+     "body_subtype" : "",
+     "doors" : "",
+     "bedlength" : "",
+     "wheelbase" : "",
+     "msrp" : "",
+     "invoice_price" : "",
+     "engine" : {
+      "description" : "",
+      "block_type" : "",
+      "cylinders" : "",
+      "displacement" : "",
+      "fuel_type" : ""
+     },
+     "transmission" : {
+      "description" : "",
+      "trans_type" : "",
+      "trans_speeds" : ""
+     },
+     "optional_equipment_codes" : "",
+     "installed_equipment_descriptions" : "",
+     "interior_color" : {
+      "description" : "",
+      "color_code" : ""
+     },
+     "exterior_color" : {
+      "description" : "",
+      "color_code" : ""
+     }
+    }
+ }
+})
 
 app.use(cors({origin: '*'}))
 app.use(bodyParser.json())
@@ -12,6 +87,24 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Server Running');
+})
+
+app.post('/api/vin-decode', (req, res) => {
+    let vinNumber = req.body.vinNumber
+    // console.log(req.body)
+
+    request.post({
+        url: `${DataOneURL}/?client_id=${clientId}&authorization_code=${authToken}&decoder_query=${dataOneQuery}`,
+        headers: {
+            "content-type": "application/json"
+          }
+        },
+        (error, response, body ) => {
+            console.log('ERROR:', error)
+            console.log('RESPONSE CODE', response.statusCode);
+            console.log('BODY', body)
+        }
+    )
 })
 
 
--- src/App.js
+++ src/App.js
@@ -4,93 +4,21 @@ import './App.css';
 
 import SampleMessage from "./sample-in";
 
-const DataOneURL = 'https://api.dataonesoftware.com/webservices/vindecoder/decode';
-const clientId = '3578';
-const authToken = '8c35d0f3436d10b99b54de831db5885f0bca1a5e';
-const dataOneQuery = JSON.stringify({
-  "decoder_settings" : {
-    "display" : "full",
-    "version" : "7.0.1",
-    "styles" : "off",
-    "common_data" : "on",
-    "common_data_packs" : {
-     "basic_data" : "on",
-     "pricing" : "off",
-     "engines" : "off",
-     "transmissions" : "off",
-     "specifications" : "off",
-     "standard_specifications" : "off",
-     "optional_specifications" : "off",
-     "standard_ancillary_specifications" : "off",
-     "optional_ancillary_specifications" : "off",
-     "installed_equipment" : "off",
-     "optional_equipment" : "off",
-     "colors" : "off",
-     "safety_equipment" : "off",
-     "warranties" : "off",
-     "fuel_efficiency" : "off",
-     "green_scores" : "off",
-     "crash_test" : "off",
-     "awards" : "off"
-     }
- },
- "query_requests" : {
-    "Request" : {
-     "vin" : "1FADP3E22DL309391",
-     "year" : "",
-     "make" : "",
-     "model" : "",
-     "trim" : "",
-     "model_number" : "",
-     "package_code" : "",
-     "drive_type" : "",
-     "vehicle_type" : "",
-     "body_type" : "",
-     "body_subtype" : "",
-     "doors" : "",
-     "bedlength" : "",
-     "wheelbase" : "",
-     "msrp" : "",
-     "invoice_price" : "",
-     "engine" : {
-      "description" : "",
-      "block_type" : "",
-      "cylinders" : "",
-      "displacement" : "",
-      "fuel_type" : ""
-     },
-     "transmission" : {
-      "description" : "",
-      "trans_type" : "",
-      "trans_speeds" : ""
-     },
-     "optional_equipment_codes" : "",
-     "installed_equipment_descriptions" : "",
-     "interior_color" : {
-      "description" : "",
-      "color_code" : ""
-     },
-     "exterior_color" : {
-      "description" : "",
-      "color_code" : ""
-     }
-    }
- }
-})
+
 
 
 class App extends Component {
 
   onSubmitHandler = (event) => {
     event.preventDefault()
-    console.log('Submit works!')
-
-    fetch(`${DataOneURL}/?client_id=${clientId}&authorization_code=${authToken}&decoder_query=${dataOneQuery}`, {
+    console.log('Input Ref', this.vinInput.value)
+    fetch('/api/vin-decode/', {
       method: 'POST',
-      mode: 'cors',
       headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json'
+      },
+      body: JSON.stringify({vinNumber: this.vinInput.value})
     }).then(response => {
       console.log('FETCH RESPONSE:', response)
       return response.json()
@@ -114,7 +42,7 @@ class App extends Component {
         </p>
 
         <form onSubmit={(event) => this.onSubmitHandler(event)}>
-          <input type="text"/>
+          <input type="text" ref={ref => this.vinInput = ref}/>
           <input type="submit"/>
         </form>
         
